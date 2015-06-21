877c9da9f2fbf8b764310d76a9ad07f88b588338
Karl Heinz Marbaise <khmarbaise@apache.org>
2015-06-21 22:04:57 +0200

    Added parameter for reactorProjects.

--- src/main/java/sample/plugin/GreetingMojo.java
+++ src/main/java/sample/plugin/GreetingMojo.java
@@ -1,16 +1,34 @@
 package sample.plugin;
 
+import java.util.List;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
-@Mojo(name = "sayhi")
-public class GreetingMojo extends AbstractMojo {
+@Mojo( name = "sayhi" )
+public class GreetingMojo
+    extends AbstractMojo
+{
 
-	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		getLog().info("Hello, world.");
-	}
+    @Parameter( defaultValue = "${reactorProjects}", required = true, readonly = true )
+    private List<MavenProject> mavenProjects;
+
+    @Override
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        getLog().info( "Hello, world." );
+        if ( mavenProjects != null )
+        {
+            for ( MavenProject mavenProject : mavenProjects )
+            {
+                getLog().info( " Project: " + mavenProject.getId() );
+            }
+        }
+    }
 
 }
