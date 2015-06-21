ac481c14354058f2cf21a9b1f554bde6c1144b48
Karl Heinz Marbaise <khmarbaise@apache.org>
2015-06-21 21:57:23 +0200

    Get Working code.

---
+++ src/main/java/sample/plugin/GreetingMojo.java
@@ -0,0 +1,16 @@
+package sample.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "sayhi")
+public class GreetingMojo extends AbstractMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		getLog().info("Hello, world.");
+	}
+
+}
