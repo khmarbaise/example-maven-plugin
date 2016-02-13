9c96dc7d02d5feca35e0078f5cf2ee5b12668eed
Karl Heinz Marbaise <khmarbaise@apache.org>
2016-02-13 15:42:16 +0100

    Changed to Maven 3.0 only.

--- src/main/java/sample/plugin/GreetingMojo.java
+++ src/main/java/sample/plugin/GreetingMojo.java
@@ -2,6 +2,7 @@ package sample.plugin;
 
 import java.util.List;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -14,21 +15,26 @@ public class GreetingMojo
     extends AbstractMojo
 {
 
-    @Parameter( defaultValue = "${reactorProjects}", required = true, readonly = true )
-    private List<MavenProject> mavenProjects;
+    @Parameter( defaultValue = "${session}", readonly = true, required = true )
+    private MavenSession session;
+
+    @Parameter( defaultValue = "${project}", readonly = true, required = true )
+    private MavenProject project;
 
     @Override
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
         getLog().info( "Hello, world." );
-        if ( mavenProjects != null )
+
+        List<MavenProject> sortedProjects = session.getProjectDependencyGraph().getSortedProjects();
+        for ( MavenProject mavenProject : sortedProjects )
         {
-            for ( MavenProject mavenProject : mavenProjects )
-            {
-                getLog().info( " Project: " + mavenProject.getId() );
-            }
+
+            getLog().info( " SortedProject: " + mavenProject.getId() + " BD:" + mavenProject.getBasedir() + " "
+                + mavenProject.getArtifact().getFile() );
         }
+
     }
 
 }
